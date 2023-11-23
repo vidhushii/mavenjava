a748d3788729aa97283388cc2c55d9986e0016e6
vidhushii <vidhushi8434@gmail.com>
2023-11-23 11:14:19 +0530

    Maven java push

---
+++ src/com/studyopedia/Demo.java
@@ -0,0 +1,11 @@
+package com.studyopedia;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Amit thinks very high");
+
+	}
+
+}
